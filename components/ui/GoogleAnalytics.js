cfe755248d9e2474606613e63e5ff2077a2bbb59
jandro29 <ascolanr@gmail.com>
2025-10-22 19:58:49 -0500

    .

---
+++ components/ui/GoogleAnalytics.js
@@ -0,0 +1,28 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  const gaId = "G-K4ESHBPG8D"; // Tu ID directo aquí
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+      />
+      <Script
+        id="google-analytics-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `,
+        }}
+      />
+    </>
+  );
+}
