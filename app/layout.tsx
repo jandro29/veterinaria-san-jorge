6200124ff7d67761d720f9639cbbed5b966e35aa
jandro29 <ascolanr@gmail.com>
2025-10-23 11:33:12 -0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import GoogleAnalytics from "../components/ui/GoogleAnalytics";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
+        <GoogleAnalytics />
         <Analytics />
       </body>
     </html>
